1433b68de4a85c1ce4be3122f6a5d6857f233ece
KoVLya <speedbreaker228@gmail.com>
2022-12-27 19:37:47 +0300

    txt parser upd

--- src/main/java/com/cross_cutting/Parsers/TXTParser/TxtParser.java
+++ src/main/java/com/cross_cutting/Parsers/TXTParser/TxtParser.java
@@ -1,102 +1,61 @@
 package com.cross_cutting.Parsers.TXTParser;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.annotation.processing.FilerException;
+import com.cross_cutting.Parsers.AriphmeticParser;
+
 public class TxtParser {
    
     private String path;
     private String outPath;
-
     
-    // private static List<Pair<String, Order>> orders = new ArrayList<Pair<String, Order>>();
+    private static ArrayList<Integer> rezuList = new ArrayList<>();
 
-    // public TXTParser(String path, String outPath) {
-    //     this.path = path;
-    //     this.outPath = outPath;
-    // }
+    public TxtParser(String path, String outPath) {
+        this.path = path;
+        this.outPath = outPath;
+    }
 
-    // public TXTParser(String path) {
-    //     this.path = path;
-    // }
+    public TxtParser(String path) {
+        this.path = path;
+    }
 
-    // public void ReadFile() {
-    //     try(BufferedReader buffer = new BufferedReader(new FileReader(path))){
+    public void ReadFile() {
+        try(BufferedReader buffer = new BufferedReader(new FileReader(path))){
 
-    //         while(buffer.ready()) {
-    //             Order tem = new Order();
+            while(buffer.ready()) {
+                String temp = buffer.readLine().toString();
+                AriphmeticParser parser = new AriphmeticParser(temp);
+                rezuList.add(parser.getResult());
+            }
 
-    //             String[] temp = buffer.readLine().split(" ");
-    //             String name = temp[0];
-
-    //             temp = buffer.readLine().split(" ");
-    //             String type = temp[0];
-
-    //             if(type.equals(MenuType.BREAKFAST.toString())){
-    //                 tem.setType(MenuType.BREAKFAST);                
-    //             }
-    //             else if(type.equals(MenuType.LUNCH.toString())){
-    //                 tem.setType(MenuType.LUNCH);
-    //             }
-    //             else if(type.equals(MenuType.DINNER.toString())){
-    //                 tem.setType(MenuType.DINNER);
-    //             }
-
-    //             temp = buffer.readLine().split(" ");
-    //             String numberTable = temp[0];
-    //             String amountOfTable = temp[1];
-
-    //             tem.setTable(new Table(Integer.valueOf(numberTable),
-    //                             Integer.valueOf(amountOfTable)));
-            
-    //             temp = buffer.readLine().split(" ");
-    //             String filling = temp[0];
-    //             String mainPart = temp[1];
-    //             String top = temp[2];
-
-    //             tem.setDessert(new Dessert(filling,
-    //                                 mainPart,
-    //                                 top));
-            
-    //             temp = buffer.readLine().split(" ");
-    //             String tempeture = temp[0];
-    //             String is_sweet = temp[1];
-    //             String strenght = temp[2];
-    //             String typeCoffeeBeans = temp[3];
-
-    //             tem.setCoffee(new Coffee(Double.valueOf(tempeture),
-    //                             Boolean.valueOf(is_sweet),
-    //                             Integer.valueOf(strenght),
-    //                             typeCoffeeBeans));
-            
-    //             temp = buffer.readLine().split(" ");
-    //             String am = temp[0];
-
-    //             tem.setAmoutOrders(Integer.valueOf(am));
-
-    //             orders.add(new Pair<String,Order>(name, new Order(tem)));
-
-    //             System.out.println(tem.toString());
-
-    //         }
-
-    //     } catch (Exception e) {
-    //         e.getStackTrace();
-    //     }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
         
-    // }
+    }
 
-    // public void WriteFile() throws IOException {
-    //     try(BufferedWriter buffer = new BufferedWriter(new FileWriter(outPath));) {
-    //         for(Pair<String,Order> id : orders) {
-    //             buffer.write(id.getFirstItem().toString() + "\n" + id.getSecondItem().toString() + "\n");
-    //         }
-    //     } catch(FilerException e){
-    //         System.out.println("Error");
-    //         e.getStackTrace();
-    //     }
-    // }
+    public void WriteFile() throws IOException {
+        try(BufferedWriter buffer = new BufferedWriter(new FileWriter(outPath));) {
 
-    // public static void main(String[] args) throws IOException {
-    //     TXTParser Main = new TXTParser("src/res/order.txt", "src/res/orderout.txt");
-    //     Main.ReadFile();
-    //     Main.WriteFile();
-    // }
+            for(var id : rezuList) {
+                buffer.write(id.toString() + "\n");
+            }
+
+        } catch(FilerException e){
+            System.out.println("Error");
+            e.getStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        TxtParser Main = new TxtParser("src/res/arifmetics.txt", "src/res/orderout.txt");
+        Main.ReadFile();
+        Main.WriteFile();
+    }
 }
