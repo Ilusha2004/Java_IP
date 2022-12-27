d3fed8c5a26851fa2a25fed9cdaeb6e82e3293ca
KoVLya <speedbreaker228@gmail.com>
2022-12-27 20:19:50 +0300

    RestAPI support

---
+++ src/main/java/com/cross_cutting/SpringAPI/Main.java
@@ -0,0 +1,13 @@
+package com.cross_cutting.SpringAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
