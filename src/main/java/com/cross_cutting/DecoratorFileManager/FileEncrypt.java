c13127fc8dba3ef9cc497a2c95b2aea4c3d592e2
KoVLya <speedbreaker228@gmail.com>
2023-01-06 19:02:31 +0300

    FileEncrypter in Decorator upd

--- src/main/java/com/cross_cutting/DecoratorFileManager/FileEncrypt.java
+++ src/main/java/com/cross_cutting/DecoratorFileManager/FileEncrypt.java
@@ -1,6 +1,15 @@
 package com.cross_cutting.DecoratorFileManager;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.Key;
+
 public class FileEncrypt extends DataDecorator {
+
+    private static Key key;
+    private static String encryptedPath;
 
     public FileEncrypt(DecoratorFileInterface dec) {
         super(dec);
@@ -8,11 +17,40 @@ public class FileEncrypt extends DataDecorator {
 
     @Override
     public void writeData(String data) {
-
+        super.writeData(data);
     }
 
     @Override
     public String readData() {
-        return null;
+        return super.readData();
     }
+
+    public void Encrypt() throws Exception {
+        Cipher cipher_encrypted = Cipher.getInstance("AES");
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        key = keyGenerator.generateKey();
+        cipher_encrypted.init(Cipher.ENCRYPT_MODE, key);
+        byte[] cipherText = cipher_encrypted.doFinal(new FileInputStream(FileSource.getPath()).readAllBytes());
+        FileOutputStream fileOutputStream = new FileOutputStream(FileSource.getPath());
+        setEncryptedPath("resourses/archiveAndEncr/encrypted_" + FileSource.getPath());
+        fileOutputStream.write(cipherText);
+        fileOutputStream.close();
+    }
+
+    public void setEncryptedPath(String encryptedPath) {
+        this.encryptedPath = encryptedPath;
+    }
+
+    public static String getEncryptedPath() {
+        return encryptedPath;
+    }
+
+    public static Key getKey() {
+        return key;
+    }
+
+    public static void setKey(Key key) {
+        FileEncrypt.key = key;
+    }
+
 }
