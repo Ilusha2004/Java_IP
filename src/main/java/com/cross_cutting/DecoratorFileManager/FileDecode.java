060e1842a32a6193383e58e353f49e6debb2c619
KoVLya <speedbreaker228@gmail.com>
2023-01-06 19:07:11 +0300

    FileDecode in Decorator upd

--- src/main/java/com/cross_cutting/DecoratorFileManager/FileDecode.java
+++ src/main/java/com/cross_cutting/DecoratorFileManager/FileDecode.java
@@ -1,5 +1,12 @@
 package com.cross_cutting.DecoratorFileManager;
 
+import javax.crypto.Cipher;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import static com.cross_cutting.DecoratorFileManager.FileEncrypt.getEncryptedPath;
+import static com.cross_cutting.DecoratorFileManager.FileEncrypt.getKey;
+
 public class FileDecode extends DataDecorator{
 
     public FileDecode(DecoratorFileInterface dec) {
@@ -8,11 +15,20 @@ public class FileDecode extends DataDecorator{
 
     @Override
     public void writeData(String data) {
-
+        super.writeData(data);
     }
 
     @Override
     public String readData() {
-        return null;
+        return super.readData();
+    }
+
+    public void Uncode() throws Exception {
+        Cipher cipher_deencrypted = Cipher.getInstance("AES");
+        cipher_deencrypted.init(Cipher.DECRYPT_MODE, getKey());
+        byte[] cipher_deencrypted_Text = cipher_deencrypted.doFinal(new FileInputStream(getEncryptedPath()).readAllBytes());
+        FileOutputStream fileOutputStream = new FileOutputStream("resourses/archiveAndEncr/uncoded_" + FileSource.getPath());
+        fileOutputStream.write(cipher_deencrypted_Text);
+        fileOutputStream.close();
     }
 }
