63767d2ae1459c730fb28359243818c13fab114f
KoVLya <speedbreaker228@gmail.com>
2023-01-06 20:11:04 +0300

    File path feature added

---
+++ src/main/java/com/cross_cutting/HelpfulThings/FilePath.java
@@ -0,0 +1,52 @@
+package com.cross_cutting.HelpfulThings;
+
+public class FilePath {
+
+    private static String name;
+    private static String encryptedPath;
+    private static String path;
+    private static String extension;
+
+    public FilePath(String path) {
+        this.path = path;
+
+        String[] arr = path.split("/");
+        String[] temp = arr[arr.length - 1].replace(".", " ").split(" ");
+
+        this.name = temp[temp.length - 2];
+        this.extension = temp[temp.length - 1];
+
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEncryptedPath() {
+        return encryptedPath;
+    }
+
+    public void setEncryptedPath(String encryptedPath) {
+        this.encryptedPath = encryptedPath;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+}
