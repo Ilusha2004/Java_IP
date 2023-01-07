074fffe13b017a10bcf45c5d1a8445860f2a2161
KoVLya <ilakovunov@gmail.com>
2023-01-07 23:04:26 +0300

    Encryptor upd

--- src/main/java/com/cross_cutting/DecoratorFileManager/FileEncrypt.java
+++ src/main/java/com/cross_cutting/DecoratorFileManager/FileEncrypt.java
@@ -8,12 +8,14 @@ import java.security.Key;
 
 public class FileEncrypt extends DataDecorator {
 
+    private static Key key;
+
     public FileEncrypt(DecoratorFileInterface dec) {
         super(dec);
     }
 
     @Override
-    public void writeData(String data) {
+    public void writeData(String data) throws Exception {
         super.writeData(data);
     }
 
@@ -23,12 +25,12 @@ public class FileEncrypt extends DataDecorator {
     }
 
     public void Encrypt() throws Exception {
+
         try {
             Cipher cipher_encrypted = Cipher.getInstance("AES");
             KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-            Key key = keyGenerator.generateKey();
+            key = keyGenerator.generateKey();
             cipher_encrypted.init(Cipher.ENCRYPT_MODE, key);
-            System.out.println(key.getEncoded());
             byte[] cipherText = cipher_encrypted.doFinal(new FileInputStream("src/res/" + FileSource.getFilePath().getName() + "." + FileSource.getFilePath().getExtension()).readAllBytes());
             FileOutputStream fileOutputStream = new FileOutputStream("src/res/archiveAndEncrypt/encrypted_" + FileSource.getFilePath().getName() + "." + FileSource.getFilePath().getExtension());
             setEncryptedPath("src/res/archiveAndEncrypt/encrypted_" + FileSource.getFilePath().getName());
@@ -45,6 +47,14 @@ public class FileEncrypt extends DataDecorator {
 
     public static String getEncryptedPath() {
         return FileSource.getFilePath().getEncryptedPath();
+    }
+
+    public static Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
     }
 
     public static void main(String[] args) throws Exception {
