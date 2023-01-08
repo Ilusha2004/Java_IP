1fbe6c593576030f0cacf6c72ba5325e51b4c454
KoVLya <ilakovunov@gmail.com>
2023-01-08 17:42:31 +0300

    FileDecode minor upd

--- src/main/java/com/cross_cutting/DecoratorFileManager/FileDecode.java
+++ src/main/java/com/cross_cutting/DecoratorFileManager/FileDecode.java
@@ -17,7 +17,7 @@ public class FileDecode extends DataDecorator{
     @Override
     public void writeData() throws Exception {
         if(getKey() != null) {
-            System.out.println("hui");
+            System.out.println("Decoder part");
             Decoding();
         }
         super.writeData();
@@ -32,9 +32,8 @@ public class FileDecode extends DataDecorator{
         Cipher cipher_deencrypted = Cipher.getInstance("AES");
         cipher_deencrypted.init(Cipher.DECRYPT_MODE, getKey());
         byte[] cipher_deencrypted_Text = cipher_deencrypted.doFinal(new FileInputStream(FileSource.getFilePath().getPath()).readAllBytes());
-        FileOutputStream fileOutputStream = new FileOutputStream("src/res/archiveAndEncrypt/uncoded_" + FileSource.getFilePath().getName() + "." + FileSource.getFilePath().getExtension());
+        FileOutputStream fileOutputStream = new FileOutputStream("src/res/archiveAndEncrypt/uncoded_" + FileSource.getFilePath().getName() + "." + FileSource.getFilePath().getFirstExtension());
         FileSource.setname(FileSource.getFilePath().getName());
-        FileSource.setextension(FileSource.getFilePath().getName());
         FileSource.setPath(new FilePath("src/res/archiveAndEncrypt/uncoded_" + FileSource.getFilePath().getName() + "." + FileSource.getFilePath().getExtension()));
         FileSource.setname(FileSource.getFilePath().getName());
         fileOutputStream.write(cipher_deencrypted_Text);
