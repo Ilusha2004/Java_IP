8f9bf4b40c8d13f91b3398242c6cb35f976a5684
KoVLya <ilakovunov@gmail.com>
2023-01-09 14:31:03 +0300

    CheckBox upd

--- GUIsup/src/main/java/com/grapfx/guisup/StageController.java
+++ GUIsup/src/main/java/com/grapfx/guisup/StageController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -18,7 +19,6 @@ public class StageController {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    private int counter = 0;
     private static Scenes scenes = new Scenes();
 
 
@@ -29,7 +29,7 @@ public class StageController {
     @FXML
     Label label;
 
-    public void Switch(ActionEvent event, int count) throws IOException {
+    public void Switch(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(scenes.getAllPath().get(scenes.getCounter())));
         root = loader.load(getClass().getResource(scenes.getAllPath().get(scenes.getCounter())));
         this.stage = (Stage)((Node) event.getSource()).getScene().getWindow();
@@ -48,14 +48,12 @@ public class StageController {
             System.out.println(ex.getMessage());
         }
 
-        Switch(event, counter);
-        System.out.println(counter);
+        Switch(event);
     }
 
     public void PrevScene(ActionEvent event) throws IOException {
         scenes.Decrease();
-        Switch(event, counter);
-        System.out.println(counter);
+        Switch(event);
     }
 
     public void Close() {
@@ -67,6 +65,29 @@ public class StageController {
     public void NewPath(ActionEvent event) throws IOException {
         String path = textField.getText();
         label.setText("Path : " + path);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    @FXML
+    CheckBox boxFir;
+    @FXML
+    CheckBox boxSec;
+    @FXML
+    Label label_1;
+
+    public void change(ActionEvent event) {
+        if(boxFir.isSelected() && boxSec.isSelected()) {
+            label_1.setText("encrypt and compress");
+            System.out.println("encrypt and compress");
+        }
+        else if(boxFir.isSelected()){
+            label_1.setText("encrypt");
+            System.out.println("encrypt");
+        }
+        else if(boxSec.isSelected()) {
+            label_1.setText("compress");
+            System.out.println("compress");
+        }
     }
 
 }
