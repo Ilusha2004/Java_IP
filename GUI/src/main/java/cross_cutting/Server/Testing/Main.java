6ef4177b2f2c39b9cf9108990d000a3def18152a
KoVLya <ilakovunov@gmail.com>
2023-01-14 21:56:36 +0300

    Test Server upd

---
+++ GUI/src/main/java/cross_cutting/Server/Testing/Main.java
@@ -0,0 +1,29 @@
+package cross_cutting.Server.Testing;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("src/res/test.txt");
+
+        Client clientSend = new Client();
+        clientSend.connectSocketServer(7890);
+
+        System.out.println("hui");
+
+        String flag = "";
+        clientSend.sendFile(file, flag);
+        clientSend.clientStop();
+
+        System.out.println("hui");
+
+        Client clientGet = new Client();
+        clientGet.connectSocketServer(7891);
+        System.out.println("Please wait");
+        clientGet.getFile();
+
+        System.out.println("Done!");
+        clientGet.clientStop();
+    }
+
+}
