19554f6e47e07a7a6a8aa964f3d8d61eba15b6d7
KoVLya <ilakovunov@gmail.com>
2023-01-15 23:53:20 +0300

    Parser Decorator fix

---
+++ src/main/java/com/cross_cutting/BuilderAriphmeticParser/BuilderParserManager.java
@@ -0,0 +1,7 @@
+package com.cross_cutting.BuilderAriphmeticParser;
+
+public interface BuilderParserManager {
+    void setParser(Parser parser);
+    void setInPath(String inPath);
+    void setOutPath(String outPath);
+}
